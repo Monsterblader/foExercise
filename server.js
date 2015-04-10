15d9cc8110c02595f7046dfc38e21135b4023527
Monsterblader <Monsterblader@gmail.com>
2015-04-10 10:08:10 -0700

    Fix paths.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-  res.sendfile('index.html');
+  res.sendfile('app/index.html');
 });
 
 app.get(/^(.+)$/, function (req, res) {
@@ -14,4 +14,3 @@ var port = process.env.PORT || 5000;
 app.listen(port, function () {
   console.log("Listening on " + port);
 });
-
