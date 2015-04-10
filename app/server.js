0b3cfbf01b9643f103c9be96eed38c6159b3c75b
Monsterblader <Monsterblader@gmail.com>
2015-04-10 10:15:11 -0700

    Add Procfile.

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-  res.sendfile('app/index.html');
+  res.sendfile('index.html');
 });
 
 app.get(/^(.+)$/, function (req, res) {
